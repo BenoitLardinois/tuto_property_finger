07031f0768475f9621efc94742da7b6dea170fe3
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-18 18:19:52 +0300

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+  
+  root to: 'public#main'
+  resources :properties
+  devise_for :accounts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
