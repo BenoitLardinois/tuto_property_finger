0b15bbeca61161a0045260711e96c3fa952c5753
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-21 14:45:13 +0300

    seventh_commit

--- app/javascript/packs/includes/properties.js
+++ app/javascript/packs/includes/properties.js
@@ -1,4 +1,10 @@
 $(function(){
+  $("#show-tel").on("click", function(){
+    var $this = $(this);
+    $this.find("span").text( $this.data("telephone") );
+  });
+
+$(function(){
   $("#toggleDetails").on("click", function(){
     var details = $("#prop-details");
     details.toggleClass("open");
@@ -7,5 +13,37 @@ $(function(){
     } else {
       $(this).text("Show less");             
     }
-  })
+  });
+  
+  $("#send-message-to-agent").on("click", function(){
+    var agent_id = $("#agent_id").val(),
+        first_name = $("#message-first-name").val(),
+        last_name = $("#message-last-name").val(),
+        email = $("#message-email").val(),
+        message = $("#message-text").val();
+    
+    $.ajax({
+      url: "/agent/message",
+      method: "POST",
+      dataType: "json",
+      data: {
+        agent_id: agent_id,
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        massage: message
+      },
+      success: function(data){
+        $('#contact-modal form').remove();
+        $('#send-message-to-agent').remove();
+        $('#contact-modal .modal-body').html("<p>Your message has been sent successfully!</p>");
+      }
+    });
+    
+  });
+  
+  $('#contact-modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var modal = $(this);
+  });
 });
